7a385afbcd8d1c966629a1b16c0b50f4ed7d134c
bearnon <bearnon@gmail.com>
2021-05-26 22:03:43 +0800

    debug sql and lost_pet csv

--- data/registration_agency.sql
+++ data/registration_agency.sql
@@ -4,15 +4,15 @@ create table registration_agency(
     agency_name varchar(50) collate utf8_unicode_ci,
     agency_address varchar(100) collate utf8_unicode_ci,
     contact_person varchar(8) collate utf8_unicode_ci,
-    phone_number varchar(20),
+    phone_number varchar(30),
     email varchar(100),
-    distance int,
+    distance varchar(10),
     primary key (agency_id)
 );
 
 load data local infile '登記機構.csv'
 into table registration_agency
 fields terminated by ','
-/* enclosed by '"' */
+enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines;
