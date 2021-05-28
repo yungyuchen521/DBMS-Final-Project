9d6b11b10116936b06a05bcb26d806a650ab1c01
yungyuchen521 <53526384+yungyuchen521@users.noreply.github.com>
2021-05-28 18:11:54 +0800

    Add files via upload

---
+++ sql_server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	const mysql = require('mysql');
+
+	const db = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "your_mysql_password",
+		database: "your_db_name"
+	});
+
+	db.connect(err => {
+		if (err) throw err;
+		console.log("Connected!");
+	});
+
+	db.query('SELECT * FROM registration_agency limit 3', (err,rows) => {
+		if(err) throw err;
+
+		//res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.send(rows);
+		console.log(rows);
+   });
+});
+
+const server = app.listen(3000, () => {
+	console.log("server running...");
+})
+
+/*
+const server = http.createServer((req, res) => {
+	// Set a response type of plain text for the response
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+
+	// Send back a response and end the connection
+	res.end(data);
+});
+
+// Start the server on port 3000
+server.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
+
+//db.destroy();*/
