1920d32fc058274fc798e622a918b846442c753a
bearnon <bearnon@gmail.com>
2021-06-02 00:31:25 +0800

    add animal_subid trigger

--- data/animal_subid_normalize.sql
+++ data/animal_subid_normalize.sql
@@ -10,3 +10,18 @@ create table animal_subid(
 insert ignore into animal_subid
 select animal_subid, animal_area_pkid, animal_shelter_pkid
 from adopt_animal;
+
+drop trigger if exists generate_subid;
+
+delimiter |
+create trigger generate_subid before insert on animal_subid
+for each row begin
+    select max(subid) into @temp from animal_subid where strcmp(subid,'103030711') = -1;
+    if @temp is null then
+        set new.subid = '0000000001';
+    else
+        set new.subid = lpad(convert(@temp,signed)+1,10,'0');
+    end if;
+end;
+|
+delimiter ;
